b2435d2f1b627f368dc7fd13d9020bb123a052da
Nickolas Leal Martins <nickolasleal6@gmail.com>
2025-04-09 11:41:48 -0300

    Aula03 - Polimorfismo

--- aula03/TelaCarro.py
+++ aula03/TelaCarro.py
@@ -3,20 +3,39 @@ import sys
 from PyQt5.QtWidgets import *
 from Carro import Carro
 from TelaVeiculo import TelaVeiculo
+from TelaCategoria import TelaCategoria
 
 class TelaCarro(TelaVeiculo):
 
-    def __init__(self, titulo="Tela de Carro"):
-        super().__init__(titulo)
+    def __init__(self, titulo="Tela de Carro", categorias = [], telaCate = TelaCategoria()):
+        self.telaCategorias = telaCate
+        self.listaCategorias = categorias
+        super().__init__(titulo)        
 
     def definirLayout(self):
         super().definirLayout()
         self.lblPortas = QLabel("Qtd Portas: ")
         self.txtPortas = QLineEdit(self) 
-        self.layout.addWidget(self.lblPortas )   
-        self.layout.addWidget(self.txtPortas )
+        self.layout.addWidget(self.lblPortas)   
+        self.layout.addWidget(self.txtPortas)
 
+        self.lblCategoria = QLabel("Categoria:")
+        self.layout.addWidget(self.lblCategoria)
+
+        self.cmbCategoria = QComboBox(self)
+        self.cmbCategoria.addItem("Selecione...", None)
+        for cat in self.listaCategorias:
+            self.cmbCategoria.addItem(cat.nome, cat)
+
+        self.btnAddCategoria = QPushButton("Adicionar Categoria", self)
+        self.btnAddCategoria.clicked.connect(self.abrirTelaCategoria)
+        self.layout.addWidget(self.btnSalvar) 
+ 
+        self.layout.addWidget(self.cmbCategoria)
     
+    def abrirTelaCategoria(self):
+        self.telaCategorias.show()
+
     def salvar(self):
         modelo = self.txtModelo.text()
         if modelo != "" :
